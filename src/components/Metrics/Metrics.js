aad9a8b8f85aeeb8b2d974a4021db007aaf1ab79
RM <roman-monk@users.noreply.github.com>
2021-06-08 23:14:34 +0800

    Treasury links

--- src/components/Metrics/Metrics.js
+++ src/components/Metrics/Metrics.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useContext } from "react";
 import { useTranslation } from 'react-i18next';
 import { Metrics } from './style';
 
 import GlobalTvlMetric from './GlobalTvlMetric';
 import TreasuryMetric from './TreasuryMetric';
 import GenericMetric from './GenericMetric';
+import { VaultsContext } from "../Context/ContextProvider";
+
+const rewardTokens = {
+  56: "BNB", 137: "MATIC"
+};
+
+const treasuryLinks = {
+  56: "https://bscscan.com/address/0x4a32de8c248533c28904b24b4cfcfe18e9f2ad01",
+  137: "https://explorer-mainnet.maticvigil.com/address/0x09EF0e7b555599A9F810789FfF68Db8DBF4c51a0/tokens"
+};
 
 export default function NavBar() {
 	const { t } = useTranslation();
+	const { chainId } = useContext(VaultsContext);
 	return (
 		<Metrics>
 			<GlobalTvlMetric link="https://app.beefy.finance/" />
@@ -16,33 +27,38 @@ export default function NavBar() {
 				metric="vaultCount"
 				link="https://app.beefy.finance/"
 			/>
-			<GenericMetric
-				name={t("Metric-DailyRewards")}
-				metric="dailyEarnings"
-				link="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c?a=0x453D4Ba9a2D594314DF88564248497F7D74d6b2C#tokenAnalytics"
-			/>
-			<GenericMetric
-				name={t("Metric-TotalRewards")}
-				metric="totalEarnings"
-				link="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c?a=0x453D4Ba9a2D594314DF88564248497F7D74d6b2C#tokenAnalytics"
-			/>
+      {chainId === 56 ?
+        <GenericMetric
+          name={t("Metric-DailyRewards")}
+          metric="dailyEarnings"
+          link="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c?a=0x453D4Ba9a2D594314DF88564248497F7D74d6b2C#tokenAnalytics"
+        /> : ""}
+      {chainId === 56 ?
+        <GenericMetric
+          name={t("Metric-TotalRewards")}
+          metric="totalEarnings"
+          link="https://bscscan.com/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c?a=0x453D4Ba9a2D594314DF88564248497F7D74d6b2C#tokenAnalytics"
+        /> : ""}
 			<GenericMetric
 				name={t("Metric-BIFIStaked")}
 				metric="stakedBifi"
 				link="https://bscscan.com/token/0xCa3F508B8e4Dd382eE878A314789373D80A5190A?a=0x453D4Ba9a2D594314DF88564248497F7D74d6b2C#tokenAnalytics"
 			/>
-			<GenericMetric
-				name={t("Metric-BIFIHolders")}
-				metric="bifiHolders"
-			/>
-			<TreasuryMetric
-				token="BIFI"
-				link="https://bscscan.com/token/0xca3f508b8e4dd382ee878a314789373d80a5190a?a=0x4a32de8c248533c28904b24b4cfcfe18e9f2ad01"
-				/>
-			<TreasuryMetric
-				token="WBNB"
-				link="https://bscscan.com/token/0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c?a=0x4a32de8c248533c28904b24b4cfcfe18e9f2ad01"
-				/>
+			{chainId === 56 ?
+				<GenericMetric
+					name={t("Metric-BIFIHolders")}
+					metric="bifiHolders"
+				/> : ""}
+      <TreasuryMetric
+        token="BIFI"
+        tokenName="BIFI"
+        link={treasuryLinks[chainId]}
+      />
+      <TreasuryMetric
+        token="WBNB"
+        tokenName={rewardTokens[chainId]}
+        link={treasuryLinks[chainId]}
+      />
 			<GenericMetric
 				name={t("Metric-BIFIPrice")}
 				metric="bifiPrice"
--- src/components/Metrics/TreasuryMetric.js
+++ src/components/Metrics/TreasuryMetric.js
@@ -3,11 +3,11 @@ import { useTranslation } from 'react-i18next';
 import { VaultsContext } from '../Context/ContextProvider';
 import BaseMetric from './BaseMetric';
 
-function TreasuryMetric({ token, link }) {
+function TreasuryMetric({ token, tokenName, link }) {
 	const { treasury } = useContext(VaultsContext);
 	const { t } = useTranslation();
 
-	return <BaseMetric name={t('Metric-Treasury', { token })} value={treasury[token]} link={link} />;
+  return <BaseMetric name={t("Metric-Treasury", { token: tokenName })} value={treasury[token]} link={link} />;
 }
 
 export default TreasuryMetric;
